cde693f6b52b2307a41a65e20a96e303fe3ecb0e
LalbaAnthony <anthonylalaba24@gmail.com>
2026-01-28 22:28:23 +0100

    Remove useless scripts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import typos from './core/typos';
-import format from './core/format';
-import markdown from './core/markdown';
 
 const humanizrr = {
     typos,
-    format,
-    markdown,
 };
 
 export default humanizrr;
--- src/playground.ts
+++ src/playground.ts
@@ -8,14 +8,3 @@ for (let i = 0; i < 10; i++) {
     text = "Live is like a box of chocolates. You never know what you're gonna get.";
     console.log(humanizrr.typos.make(text, { types: { swap: 0, map: 0.01, duplicate: 0, drop: 0 } }));
 }
-console.log("======================================================");
-
-text = "Hey 😊! This is a test string with emojis 🚀🔥 and other characters 🎉.";
-console.log(text);
-console.log(humanizrr.format.all(text));
-
-console.log("======================================================");
-
-text = "Here’s an example — with “quotes” and… special œ characters!";
-console.log(text);
-console.log(humanizrr.format.all(text));
